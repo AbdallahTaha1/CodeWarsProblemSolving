f6f3f222fc8a91edb8c88cad5795c9e2828c79a8
AbdallahTaha1 <engbody007@gmail.com>
2025-01-11 14:44:37 +0200

    Kata7Problems

---
+++ CodeWarsProblemSolving/Kata7Problems.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace CodeWarsProblemSolving
+{
+    internal class Kata7Problems
+    {
+        #region 1# Square Every Digit
+        public static int SquareDigits(int n)
+        {
+            StringBuilder result = new StringBuilder();
+            var chars = n.ToString();
+            foreach (var x in chars)
+            {
+                int digit = int.Parse(x.ToString());
+                result.Append(digit * digit);
+            }
+            return int.Parse(result.ToString());
+        }
+        #endregion
+
+
+    }
+}
