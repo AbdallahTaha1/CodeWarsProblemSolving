6b501c529a6db32d8b0db542eb3b58b5d2dcdd20
AbdallahTaha1 <engbody007@gmail.com>
2025-01-12 18:36:41 +0200

    new Kata8 problem

--- CodeWarsProblemSolving/Kata8Problems.cs
+++ CodeWarsProblemSolving/Kata8Problems.cs
@@ -2,7 +2,7 @@
 {
     static class Kata8Problems
     {
-        #region #1 Abbreviate a Two Word Name
+        #region 1# Abbreviate a Two Word Name
         // Link: https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/csharp
         public static string AbbrevName(string name)
         {
@@ -13,22 +13,34 @@
         }
         #endregion
 
-        #region #2 Beginner - Lost Without a Map
+        #region 2# Beginner - Lost Without a Map
         // Link: https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/csharp
         public static int[] Maps(int[] x) => x.Select(x => x * 2).ToArray();
         #endregion
 
-        #region #3 Convert boolean values to strings 'Yes' or 'No'.
+        #region 3# Convert boolean values to strings 'Yes' or 'No'.
         public static string boolToWord(bool word) => word ? "Yes" : "No";
         #endregion
 
-        #region #4 Counting sheep...
+        #region 4# Counting sheep...
         public static int CountSheeps(bool[] sheeps) => sheeps.Count(x => x);
         #endregion
 
-        #region #5 Opposites Attract
+        #region 5# Opposites Attract
         public static bool lovefunc(int flower1, int flower2) =>
             (flower1 + flower2) % 2 != 0 ? true : false;
+        #endregion
+
+        #region 6# Sum without highest and lowest number
+        public static int Sum(int[] numbers)
+        {
+            if (numbers == null || numbers.Length < 2) return 0;
+            Array.Sort(numbers);
+            int sum = 0;
+            for (int i = 1; i < numbers.Length - 1; i++)
+                sum += numbers[i];
+            return sum;
+        }
         #endregion
 
 
